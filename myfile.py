6ddf8ee16c3634ca2dec0572378f0a96b664a101
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-18 10:18:00 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,7 +5,8 @@ from datetime import time
 import datetime
 
 
-st.title("Proyecto de Programacion Avanzada 2022-2")
+st.title("Fallecidos Covid-19")
+st.subheader("Introducción")
 st.write("El covid-19 cambió todo y afectando a millones de personas. El 11 de marzo de 2020, la Organización Mundial de la Salud declaró el brote de COVID-19 como una pandemia, ya que se ha extendido simultáneamente a más de 100 países en todo el mundo. El 16 de marzo de 2020, el expresidente Martín Vizcarra declaró emergencia sanitaria en Perú y prohibió las reuniones públicas debido a la pandemia del Covid.")
 
 agendar = st.slider("Programe la asesoria:", value=(time(11,30), time(12,45)))
