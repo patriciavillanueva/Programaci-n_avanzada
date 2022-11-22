b00dacd29be4631238c99933e2c04af420c973e8
ElizabethRosmery <117460322+ElizabethRosmery@users.noreply.github.com>
2022-11-21 20:00:25 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,4 +1,24 @@
 #$ pip install streamlit --upgrade
+import streamlit as st
+import urllib.request
+import pandas as pd
+import numpy as np
+st.header("CATALOGO SISMICO 1960-2021 (IGP)")
+@st.experimental_memo
+
+def download_data():
+   url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
+   filename="Catalogo1960_2021.xlsx"
+   urllib.request.urlretrieve(url,filename)
+   df=pd.read_csv('Catalogo1960_2021.xlsx')
+   return df
+c=download_data()
+st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
+st.dataframe(c)
+st.subheader("Características del Dataset")
+st.write(c.describe())
+
+#$ pip install streamlit --upgrade
 import urllib.request
 import streamlit as st
 import pandas as pd
