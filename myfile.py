a08b8194dce05a6cf1f8b0a392dcf875eb86301a
shutyourquiznak <112133828+shutyourquiznak@users.noreply.github.com>
2022-11-21 20:33:56 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,7 +7,7 @@ st.header("Fallecidos por COVID - 19 [Ministerio de Salud MINSA]")
 #@st.experimental_memo
 
 st.subheader("**covid**")    
-video_file = open('Coronavirus Covid-19: Claves para entender la enfermedad y protegerse - Clínica Alemana.mp4', 'rb')
+video_file = open('Coronavirus Covid-19_ Claves para entender la enfermedad y protegerse - Clínica Alemana.mp4', 'rb')
 video_bytes = video_file.read()
 st.video(video_bytes)
 st.write("**Fuente**: Clínica Alemana. (2020). https://www.youtube.com/watch?v=vlzxSleRnmg")
