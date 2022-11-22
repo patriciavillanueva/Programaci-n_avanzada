8f561bf4e89e94de344f83ec13ec9ad510a54263
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-21 20:32:53 -0500

    Update dayana.py

--- dayana.py
+++ dayana.py
@@ -1,10 +1,10 @@
 #$ pip install streamlit --upgrade
-importar streamlit como st
-importar pandas como pd
-importar numpy como np
-importar altair como alt
-importar urllib.request
-importar base64
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import urllib.request
+import base64
 
 st.sidebar.header("Entradas del usuario")
 año_seleccionado=st.sidebar.selectbox('Edad', list(reversed(range(0,117))))
