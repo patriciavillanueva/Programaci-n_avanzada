45110214e6f2f90c17fd2442c237b8335021c967
ElizabethRosmery <117460322+ElizabethRosmery@users.noreply.github.com>
2022-11-21 23:00:41 -0500

    Create Imagopen.py

---
+++ Imagopen.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+#CRITERIOS
+imageCRITERIOS = Image.open('CRITERIOS.jpg')
+st.image(imageCRITERIOS)
+
+#IMAGEN PORTADA
+imagenportada = Image.open('CRITERIOS.jpg')
+st.image(imagenportada)
+
+
