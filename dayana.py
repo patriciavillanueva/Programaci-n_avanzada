fb041fe3d70c5dc3d42cdaeef23a60641087611f
DayanaHV <117460854+DayanaHV@users.noreply.github.com>
2022-11-25 08:07:29 -0500

    Update dayana.py

--- dayana.py
+++ dayana.py
@@ -18,31 +18,3 @@ st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' c
 st.dataframe(c)
 st.subheader("Características del Dataset")
 st.write(c.describe())
-
-def load_data(year):
-	df = download_data()
-	df=df.astype({'EDAD_DECLARADA':'str'})
-	grouped = df.groupby(df.EDAD_DECLARADA)
-	df_EDAD = grouped.get_group(EDAD)
-	return df_EDAD
-
-data_by_EDAD=load_data(str(selected_EDAD))
-
-sorted_unique_depa=sorted(data_by_EDAD.DEPARTAMENTO.unique())
-
-selected_depa=st.sidebar.multiselect('Departamento',sorted_unique_depa,sorted_unique_depa)
-
-unique_sexo=["FEMENINO","MASCULINO"]
-selected_sexo=st.sidebar.multiselect('SEXO', unique_sexo, unique_sexo)
-
-df_selected=data_by_EDAD[(data_by_EDAD.DEPARTAMENTO.isin(selected_depa))]
-
-def remove_columns(dataset, cols):
-	return dataset.drop(cols, axis=1)
-
-cols=np.setdiff1d(unique_sexo, selected_sexo)
-
-st.subheader('Mostrar data de departamento(s) y sexo(s) seleccionado(s)')
-data=remove_columns(df_selected, cols)
-st.write('Dimensiones: ' + str(data.shape[0]) + ' filas y ' + str(data.shape[1]) + ' columnas')
-st.dataframe(data)
